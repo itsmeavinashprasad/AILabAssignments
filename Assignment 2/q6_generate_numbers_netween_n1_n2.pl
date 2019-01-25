e7fa89e996648b7418e6af735bf090c3bf1a534d
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-01-26 02:29:39 +0530

    Assigment2 question 6 corrected

--- Assignment 2/q6_generate_numbers_netween_n1_n2.pl
+++ Assignment 2/q6_generate_numbers_netween_n1_n2.pl
@@ -1,13 +1,8 @@
-/*append*/
-append([], L, L).
-
-append([X | Rest], L1, [X | L2]):-
-	append(Rest, L1, L2).
-
 /*generate function*/
-gen(X,X,[X]).
+gen(X,X,[X]):-
+	!.
 
-gen(N1,N2,L):-
+gen(N1, N2, L):-
 	N3 is N1+1,
-	gen(N3,N2,L1),
-	L is [N1 | L1].
+	gen(N3, N2, L1),
+	L = [N1 | L1].
