fe4c061295c03492e353bf9f334c5c8d7ca65afa
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-02-07 21:40:25 +0530

    updated q13

--- Assignment 2/q13_sqrt.pl
+++ Assignment 2/q13_sqrt.pl
@@ -1,26 +1,3 @@
-/* To recursively add two integers */
-rec_add(X, 0, X):-
-	!.
-
-rec_add(N1, N2, Result):-
-	N2_2 is N2-1,
-	rec_add(N1, N2_2, Result_1),
-	Result is Result_1+1.
-
-/* To multiply two integers recursively */
-rec_mul(X, 0, 0):-
-	!.
-rec_mul(0, X, 0):-
-	!.
-
-rec_mul(X, 1, X):-
-	!.
-
-rec_mul(N1, N2, Result):-
-	N2_2 is N2-1,
-	rec_mul(N1, N2_2, Result_1),
-	rec_add(N1, Result_1, Result).
-
 /* To find the square root of an integer */
 
 sqrt(N, Result):-
@@ -29,12 +6,12 @@ sqrt(N, Result):-
 	Result is Result_1.
 
 sqrt_2(N, Probe, Result):-
-	rec_mul(Probe, Probe, Result_1),
+	Result_1 is Probe*Probe,
 	Result_1 > N,
 	Result is Probe-1,!.
 
 sqrt_2(N, Probe, Result):-
-	rec_mul(Probe, Probe, Result_1),
+	Result_1 is Probe*Probe,
 	Result_1 =< N,
 	Probe_1 is Probe+1,
 	sqrt_2(N, Probe_1, Result_2),
