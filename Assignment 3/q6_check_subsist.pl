8f65ea7736ab7460935464caaea96f5365704e29
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-02-08 11:46:19 +0530

    Assignment 3 - q6 updated

--- Assignment 3/q6_check_subsist.pl
+++ Assignment 3/q6_check_subsist.pl
@@ -7,15 +7,17 @@ subList([], _).
 
 /* first match found, then recursively check for others */
 subList([X|A], [X|B]):- 
-	my_prefix(A, B), !.
+	subList_2(A, B), !.
 
 
 /* if first elements are different, then skip till first match found */
+/* Y is discarded from 2nd list, but X is not discarded from 1st list  */
 subList([X|A], [Y|B]):- 
 	subList([X|A], B).
 
 
-/*  */
+/* assumed that already first match has been found, then X must present in 2nd list */
+/* untill 1st list becomes empty */
 subList_2([], _).
 subList_2([X|L], [X|M]):- 
 	subList_2(L, M).
