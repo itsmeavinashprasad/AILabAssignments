148442e095beba9851d7adc15b4cb603509d3ded
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-02-15 12:34:50 +0530

    Assignment 4 : q11 updated

--- Assignment 4/q11_exchange_first_last.pl
+++ Assignment 4/q11_exchange_first_last.pl
@@ -1,6 +1,15 @@
 /* consult(q5_reverse_using_acc) */
 /* exchange_first_last(L, L1), defines that L1 to be obtained from L with first and last elements exchanged */
 
+exchange_first_last([], []):-
+	!.
+
+exchange_first_last([X], [X]):-
+	!.
+	
+exchange_first_last([X|[Y]], [Y|[X]]):-
+	!.
+	
 exchange_first_last( [First | Rest], L1):-
 	reverse(Rest, [Last | Rest2]),
 	L2 = [First | Rest2],
