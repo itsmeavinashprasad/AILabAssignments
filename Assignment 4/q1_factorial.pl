f8309a738572fe09e7e1ad2bd7068f840ff7e9fe
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-02-15 10:49:55 +0530

    Assignment 4 : q1 updated

---
+++ Assignment 4/q1_factorial.pl
@@ -0,0 +1,13 @@
+% wrapper
+fact(N, Result):-
+	fact_acc(N, 1, Result).
+
+% break condition
+fact_acc(0, Ac, Ac):-
+	!.
+
+fact_acc(N, Ac, Result):-
+	N > 0,
+	N_1 is N-1,
+	Ac_1 is Ac*N,
+	fact_acc(N_1, Ac_1, Result).
