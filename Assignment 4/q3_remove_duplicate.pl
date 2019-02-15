e1ec840a57ef5f2ed05e256968b75ca3be1d2f66
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-02-15 11:28:55 +0530

    Assignment 4 : q3 updated

--- Assignment 4/q3_remove_duplicate.pl
+++ Assignment 4/q3_remove_duplicate.pl
@@ -4,10 +4,18 @@ our_member(X, [X|_]):-
 our_member(X, [_|Rest]):- 
 	our_member(X, Rest).
 
+/* reverse result */
+reverse([], Ac, Ac):-
+	!.
+reverse([Head | Rest], Ac, L):-
+	reverse(Rest, [Head|Ac], L).
+
+
 /* To remove duplicate elements from a list using accumulator */
 
 rem_dup(L1, L):-
-	rem_dup_acc(L1, [], L).
+	rem_dup_acc(L1, [], L2),
+	reverse(L2, [], L).
 
 rem_dup_acc([], Ac, Ac):-
 	!.
