f5b674cfae5b0811d4f062def3d52867c379d77e
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-02-22 15:47:16 +0530

    Assignment 4: q13 updated

--- Assignment 4/q13_circular_right_shift.pl
+++ Assignment 4/q13_circular_right_shift.pl
@@ -1,4 +1,7 @@
-/* circular_right_shift(L, L1) */
+/* circular_left_shift(L, L1). 			*/
+/* That is,								*/
+/* 	if L= [a, b, c, d, e, f] then		*/
+/* 	L1= [f, a, b, c, d, e,]				*/
 
 circular_right_shift([], []):-
 	!.
