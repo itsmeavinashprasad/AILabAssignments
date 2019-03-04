ac6353bb1a406186306435583940bdad3ea613de
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-03-04 20:59:14 +0530

    question 6 merge sort updated

---
+++ Assignment 5/q6_quick_sort_using_accumulator.pl
@@ -0,0 +1,27 @@
+quick_sort(L, L1):-
+	quick_acc(L, [], L1).
+
+
+
+
+quick_acc([],A,A):-
+	!.
+quick_acc([X|Rest], A, L):-
+	partition(Rest, X, Lt, Gt),
+	quick_acc(Gt, A, GtSorted),
+	quick_acc(Lt, [X|GtSorted], L).
+
+
+
+
+partition([],_,[],[]):-
+	!.
+
+partition([H|Rest], X, [H|Lt], Gt):-
+	H =< X ,
+	!,
+	partition(Rest, X, Lt, Gt).
+
+partition([H|Rest], X, Lt, [H|Gt]):-
+	H > X ,
+	partition(Rest, X, Lt, Gt).
