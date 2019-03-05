aa82247c2738a22cb695b74521a3f58f65db8622
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-03-05 06:45:05 +0530

    added 8 queen problem

---
+++ 8_queen_problem.pl
@@ -0,0 +1,18 @@
+template([X1/1, X2/2, X3/3, X4/4, X5/5, X6/6, X7/7, X8/8]).
+
+solution([]).
+solution([X/Y|Others]):-
+	our_member(X, [1,2,3,4,5,6,7,8]),
+	solution(Others),
+	no_attack(X/Y, Others).
+
+no_attack(_,[]).
+no_attack(X/Y, [X1/Y1|Others]):-
+	X=\=X1,
+	(X-X1)=\=(Y-Y1),
+	(X-X1)=\=(Y1-Y),
+	no_attack(X/Y, Others).
+
+our_member(X,[X|Rest]).
+our_member(X, [Y|Rest]):-
+	our_member(X, Rest).
